e88071d5b1164c8d05d655e5d9a293fa2f83e12f
Anthony CARRÉ <carre@mr079031.spm.univ-rennes1.fr>
2015-12-01 09:13:20 +0100

    add soustraction first release, must be included with addition

---
+++ soustraction.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Anthony CARRÉ
+Révision de tables d’addition
+"""
+from random import randint
+import timeit
+
+class color:
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   RED = '\033[91m'
+   END = '\033[0m'
+
+points=0 # initialisation du score
+num_question=1 # initialisation du compte du numéro de question
+
+print(color.BLUE)
+print("#############  Tables d’addition  #############",color.END)
+print("Laisser vide la réponse et valider pour arrêter\n")
+
+start = timeit.default_timer() # initialisation du chrono
+
+while num_question<100:
+
+    x=(randint(1,9))
+    y=(randint(1,9))
+    z=x+y
+    print("---------",color.BLUE,str(num_question),color.END,"---------")
+    equation=str(z)+" - "+str(y)+" = "
+    input_reponse = input(equation)
+    
+    # il n’y a pas réponse
+    if not input_reponse:
+        num_question=num_question-1
+        if num_question==0: quit()
+        break
+   
+    # il y a une réponse
+    if (x==int(input_reponse)):
+        print(color.GREEN,"😋",color.END)
+        points=points+1
+    else:
+        print(color.RED,"😥 ",z,"-",y,"=",z,color.END)
+
+    num_question=num_question+1
+
+stop = timeit.default_timer() # arrêt du chrono
+chrono=stop-start
+temps_moyen_par_question=chrono/num_question
+nb_fautes=num_question-points
+score=int( 1000* (points-0.2*nb_fautes)/temps_moyen_par_question )
+
+print("\n#############################")
+print(color.BLUE,"Bonnes réponses : ",points,"/",num_question)
+print(" Temps :", round(chrono,2)," secondes")
+print(" Score :", score," points\n",color.END)
